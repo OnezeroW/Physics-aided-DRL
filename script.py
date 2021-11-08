319c5698bf57d63afb4a8b2fbbf38f3d6a7b6a33
OnezeroW <33685066+OnezeroW@users.noreply.github.com>
2021-11-08 10:02:03 -0700

    Update script.py

--- script.py
+++ script.py
@@ -3,4 +3,4 @@ import os
 for i in range(25):
     file = str((i+1)*2000)
     print(file)
-    os.system('python Trained-model.py --model actor-r-4-gamma-0.99-init-1-eval-%s --findex 1 > tm.log 2>&1 &' % (file))
+    os.system('python Trained-model.py --model actor-r-3-gamma-0.99-init-1-eval-%s --findex 1 > tm.log 2>&1 &' % (file))
